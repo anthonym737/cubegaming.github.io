32f95b5629fff26eb27578b8a6c9ac4ac3963f0a
anthonym737 <anthonymorand737@gmail.com>
2020-08-12 17:28:18 +0200

    bons plans cube gaming

---
+++ liensbonsplans/BonPlanCODMWXboxOne.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Call Of Duty : Modern Warfare</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+			
+			<?php include("../header_two.php"); ?>
+
+			<section id="produit">
+				<h2 class="titre_produit">Bon plan</h2>
+				<div id="contenu">	
+					<div class="photo_detail_produit_jeu">
+						<img src="../images/bons_plans/cod_mw_xboxone.jpg" alt="Photo du jeu Call Of Duty : Mordern Warfare Xbox One" />
+					</div>
+					<div class="presentation_detail_produit_jeu_cod_mw">
+						<h1>Call Of Duty : Mordern Warfare - Xbox One</h1>
+						<div class="contenu_prix_produit_jeu">
+							<div iclass="prix_produit_jeu">
+								<p><div id="prix_barre_produit_bon_plan">49.99€</div> <div id="prix_non_barre_produit_bon_plan">35.99€ (Bon plan -28%)</div></p>
+							</div>
+						</div>
+						<div id="description_detail_produit_bon_plan">
+							<p>Préparez-vous pour le retour de Modern Warfare® !<a href="#LienLireLaSuite">Lire la suite.</a></p> 
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_jeu">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de parution :</strong> 25 Octobre 2019 <br />
+					   - <strong>Genre :</strong> Jeu de guerre</p>
+
+					<h3 id="description_sup_jeu">Description</h3>
+					<div id="description_suite">
+						<p>Préparez-vous pour le retour de Modern Warfare® ! <br />
+						Dans un tout nouvel opus aux enjeux plus élevés que jamais, les joueurs incarneront des agents d’élite des forces spéciales pris dans l’engrenage haletant d’un conflit à l’échelle globale qui menace l'équilibre du pouvoir. Call of Duty®: Modern Warfare® entrainera les joueurs dans une expérience à l'intensité sans pareille, brute, sombre et provocatrice ; mettant en avant la nature changeante de la guerre contemporaine. Développé par le studio Infinity Ward à l’origine de la série, Call of Duty: Modern Warfare propose une expérience épique, réimaginée de fond en comble. <br />
+						À travers une Campagne solo viscérale et sans concessions, Call of Duty: Modern Warfare repousse les limites de la série vers des sommets que seul Modern Warfare est capable d'atteindre. Aux côtés d’agents des forces spéciales internationales et de combattants de la liberté, les joueurs prendront part à des opérations secrètes qui les conduiront aux quatre coins du globe, dans des villes emblématiques d'Europe et jusqu’aux confins du Moyen-Orient. <br />
+						Et l’histoire ne fait que commencer. <br />
+						Dans Call of Duty: Modern Warfare, les joueurs seront plongés dans une trame narrative palpitante qui se prolongera à travers tous les aspects du jeu. Plongez dans l'expérience en ligne ultime avec le célèbre mode Multijoueur adoubé par les fans, ou prenez part à une série d’opérations d'élite en coopération accessibles pour les joueurs de tous niveaux.</p>
+
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+						<p>Points forts : <br />
+						- Dans une Campagne solo viscérale et sans concession aux enjeux plus élevés que jamais, combattez aux côtés d'agents d’élite des forces spéciales pris dans un engrenage haletant qui menace d'affecter l'équilibre mondial. <br />
+						- Plongez dans l'expérience en ligne ultime avec le mode Multijoueur signature. <br />
+						- Prenez part à une série d’opérations d'élite en coopération accessibles pour les joueurs de tous niveaux. <br />
+
+
+						Contenus additionnels : <br />
+						PRÉCOMMANDEZ ET OBTENEZ UNE FIGURINE COLLECTOR EXCLUSIVE CAPTAIN PRICE + UN BONUS DE DOUBLE XP* <br />
+
+						- RÉSERVEZ ET OBTENEZ UN BONUS DE [2XP]* <br />
+
+						Précommandez Call of Duty®: Modern Warfare® et obtenez 3 heures de Double XP*. <br />
+
+						- Figurine du Capitaine Price de Call of Duty: Modern Warfare par McFarlane Toys <br />
+
+						La figurine du Capitaine Price de Call of Duty: Modern Warfare par McFarlane Toys représente le personnage emblématique de la franchise, vêtu de son treillis couleur désert inspiré du tout nouveau jeu Call of Duty: Modern Warfare* et reposant sur un socle stylisé. <br />
+
+						- Figurine exclusive de 8 cm du Capitaine Price, design inspiré par le tout nouveau jeu Call of Duty: Modern Warfare. <br />
+						- Figurine du Capitaine Price vêtu de son treillis couleur désert sur un socle stylisé. <br />
+						- Figurine exclusive offerte pour toute précommande du jeu Call of Duty: Modern Warfare chez Micromania*. <br />
+
+						*Visuel non contractuel. Dans la limite des stocks disponibles. Contactez votre revendeur pour plus d'informations.</p>	
+					</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+
+		</div>
+	</body>
+</html>
