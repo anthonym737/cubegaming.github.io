6f2c108e2bca418126c1831b20f367e1b9686342
anthonym737 <anthonymorand737@gmail.com>
2020-08-12 17:44:03 +0200

    dossier top ventes cube gaming

---
+++ lienstopventes/JeuFinalFantasyVIIRemakeDeluxeEditionPS4.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Jeu Final Fantasy VII 'Remake' Deluxe Edition Playstation 4</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+			<?php include("../header_two.php"); ?>
+			
+			<section id="produit">
+				<h2 class="titre_produit_jeu">Produit</h2>
+				<div id="contenu">
+					<div class="photo_detail_produit_jeu">
+						<img src="../images/jeux_video/ps4/final_fantasy_VII_remake_deluxe_edition_ps4.jpg" alt="Photo du jeu Final Fantasy VII Remake PS4" />
+					</div>
+					<div class="presentation_detail_produit_jeu_FFVII_remake">
+						<h1>Final Fantasy VII 'Remake' Deluxe Edition PS4</h1>
+						<div class="contenu_prix_produit_jeu">
+							<div class="prix_produit_jeu">
+								<p>59.99€</p>
+							</div>
+						</div>
+						<div id="description">
+							<p>Le monde est sous le contrôle de la Shinra, une compagnie hégémonique qui produit de l'énergie en exploitant la "mako", l'énergie vitale de la planète.<a href="#LienLireLaSuite">Lire la suite</a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_jeu">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de sortie :</strong> 10 Avril 2020 <br />
+					   - <strong>Genre :</strong> Jeu de rôle</p>
+
+					<h3 id="description_sup_jeu">Description</h3>
+					<div id="description_suite">
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+					</div>
+				</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+			
+		</div>
+	</body>
+</html>
---
+++ lienstopventes/JeuOnePiecePirateWarriors4PS4.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Jeu One Piece Pirate Warriors 4 Playstation 4</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+			<?php include("../header_two.php"); ?>
+
+			<section id="produit">
+				<h2 class="titre_produit_jeu">Produit</h2>
+				<div id="contenu">
+					<div class="photo_detail_produit_jeu">
+						<img src="../images/jeux_video/ps4/one_piece_pirate_warriors_4_ps4.jpg" alt="Photo du Jeu One Piece Pirate Warriors 4 PS4" />
+					</div>
+					<div class="presentation_detail_produit_jeu">
+						<h1>One Piece Pirate Warriors 4 - PS4</h1>
+						<div class="contenu_prix_produit_jeu">
+							<div class="prix_produit_jeu">
+								<p>49.99€</p>
+							</div>
+						</div>
+						<div id="description">
+							<p>Les Pirate Warriors sont de retour ! Découvrez une histoire encore plus explosive, de nouveaux environnements et des attaques toujours plus folles dans ONE PIECE: Pirate Warriors 4.<a href="#LienLireLaSuite">Lire la suite</a></p>
+						</div>
+					</div>	
+				</div>
+			</section>
+
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_jeu">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de sortie :</strong> 27 Mars 2020 <br />
+					   - <strong>Genre :</strong> Action/Aventure</p>
+
+					<h3 id="description_sup_jeu">Description</h3>
+					<div id="description_suite">
+						<p>Les Pirate Warriors sont de retour ! Découvrez une histoire encore plus explosive, de nouveaux environnements et des attaques toujours plus folles dans ONE PIECE: Pirate Warriors 4. D’île en île, accompagnez Luffy et l’équipage du Chapeau de Paille dans leur quête du trésor légendaire : le One Piece. À travers une extraordinaire aventure, retracez l’histoire du manga, évoluez parmi les environnements les plus impressionnants de la série et défiez de féroces ennemis. <br />
+
+						Choisissez parmi vos personnages préférés et combattez d'innombrables ennemis lors de batailles emblématiques sorties tout droit de l'anime One Piece. Développé par KOEI TECMO GAMES, les maîtres du genre Action Musou, ONE PIECE: Pirate Warriors 4 associe le plaisir de terrasser des hordes d’ennemis au cours d'intenses combats à la personnalité haute en couleur des personnages de la série. Tenez-vous prêts à embarquer pour une nouvelle aventure Pirate Warriors !</p>
+
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+						<p>Une histoire encore plus complète <br />– Découvrez l'histoire de ONE PIECE depuis son commencement <br />
+						Un jeu d'Action Musou explosif – Développé par KOEI TECMO GAMES, les maîtres du genre Action Musou <br />
+						Une victoire sans compromis <br />– Affrontez d'innombrables ennemis au cours d'immenses batailles tirées des moments emblématiques de l'anime <br />
+						Un environnement destructible <br />– Réduisez en miettes les bâtiments et autres éléments qui vous entourent pour remporter la victoire </p>
+					</div>
+				</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+			
+		</div>
+	</body>
+</html>
