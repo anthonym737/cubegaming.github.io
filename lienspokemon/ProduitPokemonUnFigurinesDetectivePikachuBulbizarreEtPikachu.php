e6391e41423c9ea0f557647d82b596103425638e
anthonym737 <anthonymorand737@gmail.com>
2020-08-12 17:39:33 +0200

    dossier pokemon cube gaming

---
+++ lienspokemon/ProduitPokemonUnFigurinesDetectivePikachuBulbizarreEtPikachu.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Figurines Detective Pikachu - Bulbizarre & Pikachu</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+			<?php include("../header_two.php"); ?>
+
+			<section id="produit">
+				<h2 class="titre_produit">Produit</h2>
+				<div id="contenu">
+					<div class="photo_detail_produit_produit_derive">
+						<img src="..\images\pokemon\figurine_pokemon_detective_pikachu_3_5cm_et_8cm.jpg" id="photo_figurine_pokemon_detective_bulbizare_et_pikachu">
+					</div>
+					<div class="presentation_detail_produit_produit_derive_modif_bp">
+						<h1>Figurine Pokemon Detective - Bulbizarre et Pikachu - 3-5 cm et 8 cm</h1>
+						<div class="contenu_prix_produit_produit_derive">
+							<div class="prix_produit_produit_derive">
+								<p>5.99€</p>
+							</div>
+						</div>
+						<div id="description_produit_produit_derive">
+							<p>Figurines en PVC de Pikachu et Bulbizarre dans la collection des figurines du film Détective Pikachu. Taille : entre 3 et 8 cm selon les personnages.<a href="#LienLireLaSuite">Lire la suite</a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_produit_derive">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de parution :</strong>9 Septembre 2019 <br />
+					   - <strong>Genre :</strong>Produit dérivé</p>
+
+					<h3 id="description_sup_produit_derive">Description</h3>
+					<div id="description_suite">
+						<p>Pikachu est sans doute le plus connu des Pokémon, ces monstres qui ont envahi la France en 1998. Ce petit écureuil jaune est connu pour lancer des décharges d’électricité dans des occasions diverses. Mascotte officielle de la licence Pokémon, il a eu l'honneur de consoles à son effigie, comme notamment la Game Boy Color et la Nintendo 64. Aujourd'hui, il est le héros du film Détective Pikachu, dans lequel il fait équipe avec Tim Goodman, un jeune étudiant venant de rentrer à Ryme City, une ville où Pokémon et humains et dresseurs vivent en paix, pour mener plusieurs enquêtes criminelles. <br />
+
+	Découvrez les nouvelles figurines Pokémon du film Détective Pikachu ! Vous y trouverez à coup sûr vos Pokémon préférés du film. Ce pack regroupe Pikachu coiffé de son chapeau de détective et Bulbizarre, soit deux des plus anciens et populaires Pokémon !</p>
+
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+						<p>Matériau : PVC. <br />
+	Taille : entre 3 et 8 cm selon les personnages.</p>
+					</div>
+				</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+			
+		</div>
+	</body>
+</html>
