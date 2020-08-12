2cd30fcb9113fa623c74b658f1819ccc333bd18c
anthonym737 <anthonymorand737@gmail.com>
2020-08-12 17:40:59 +0200

    dossier pop (figurines) cube gaming

---
+++ lienspop/TendancePopFigurineFunkoPop348StarWarsMandalorianMandalorienHeavyInfantry.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Figurine Funko Pop Star Wars Mandalorian - Mandalorien Heavy Infantry</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+			<?php include("../header_two.php"); ?>
+
+			<section id="produit">
+				<h2 class="titre_produit">Produit</h2>
+				<div id="contenu">
+					<div class="photo_detail_produit_produit_derive">
+						<img src="..\images\nouveautes\figurine_funko_pop_348_star_wars_mandalorian_mandalorien_heavy_infantry.jpg" alt="Photo d'un produit dérivé Figurin Funko Pop Star Wars Mandalorian Mandalorien Heavy Infantry">
+					</div>
+					<div class="presentation_detail_produit_produit_derive_modif_swmhi">
+						<h1>Figurine Funko Pop - Star Wars Mandalorian - Mandalorien Heavy Infantry</h1>
+						<div class="contenu_prix_produit_produit_derive">
+							<div class="prix_produit_produit_derive">
+								<p>7.99€</p>
+							</div>
+						</div>
+						<div id="description_detail_produit_produit_derive">
+							<p>La plus grande collection de figurines Pop Funko est disponible chez Cube-Gaming. Amusez-vous à chercher votre licence préférée. Cette figurine Pop en PVC mesure 9 cm de hauteur.<a href="#LienLireLaSuite">Lire la suite</a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_produit_derive">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de parution :</strong> 10 Mars 2020 <br />
+					   - <strong>Genre :</strong> Produit dérivé</p>
+
+					<h3 id="description_sup_produit_derive">Description</h3>
+					<div id="description_suite">
+						<p>Chez Cube-Gaming, depuis plusieurs années, des milliers de figurines Funko Pop sont disponibles. Elles sont parfois proposées en exclusivité ! <br />
+
+	Amusez-vous à rechercher vos personnages préférés. Le catalogue complet est incroyable ! <br />
+
+	Vous trouverez facilement votre univers préféré ou votre personnage favori  (Manga, DC Comics, Marvel, séries TV, films, musique...).</p>
+
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+						<p>À chaque Pop, son numéro. C'est parti pour posséder la plus belle des collections. <br />
+
+	- Taille : 9 cm <br />
+	- Matière : PVC <br />
+
+	Cette figurine est "bobble head", c'est-à-dire que la tête est disproportionnée par rapport au corps et montée sur ressort. Quand vous posez la figurine sur une table par exemple, le corps ne bouge pas, mais la tête s'agite un peu.</p>
+					</div>
+				</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+			
+		</div>
+	</body>
+</html>
---
+++ lienspop/TendancePopFigurineFunkoPopRickEtMortyRickAuxToilettes.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../CreationCSS.css" />
+		<title>Cube-Gaming - Figurine Rick et Morty - Rick Aux Toilettes</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+			<?php include("../header_two.php"); ?>
+
+			<section id="produit">
+				<h2 class="titre_produit">Produit</h2>
+				<div id="contenu">
+					<div class="photo_detail_produit_produit_derive">
+						<img src="..\images\nouveautes\figurine_funko_pop_694_rick_et_morty_rick_aux_toilettes.jpg" alt="Photo d'un produit dérivé Figurine Funko Pop Rick et Morty Rick Aux Toilettes">
+					</div>
+					<div class="presentation_detail_produit_produit_derive_remrat">
+						<h1>Figurine Funko Pop - Rick et Morty - Rick aux toilettes</h1>
+						<div class="contenu_prix_produit_produit_derive">
+							<div class="prix_produit_produit_derive">
+								<p>16.99€</p>
+							</div>
+						</div>
+						<div id="description_detail_produit_produit_derive">
+							<p>La plus grande collection de figurines Pop Funko est disponible chez Cube-Gaming. Amusez-vous à chercher votre licence préférée. Cette figurine Pop en PVC mesure 9 cm de hauteur.<a href="#LienLireLaSuite">Lire la suite</a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+			<section>
+				<h2 id="LienLireLaSuite">Détails supplémentaires sur le produit</h2>
+				<div class="contenu_detail_sup_produit_produit_derive">
+					<h3 id="details">Details sur le produit</h3>
+					<p>- <strong>Date de parution :</strong> 4 Décembre 2019 <br />
+					   - <strong>Genre :</strong> Produit dérivé</p>
+
+					<h3 id="description_sup_produit_derive">Description</h3>
+					<div id="description_suite">
+						<p>Si vous êtes un adulte, vous devez sans doute connaître Rick Sanchez, le scientifique alcoolique, irresponsable, vulgaire et cynique, et Morty Smith, 14 ans, son petit-fils qui le suit dans toutes ses aventures spatiales délirantes. Ils ont été créés en 2013 avec le reste de leur famille - et de nombreux monstres et extraterrestres - pour une série animée plutôt trash où humour noir et scènes de violence sont fréquents. Pastiches de séries ou de films (Total Rickall, par exemple), scènes délirantes à foison sur des planètes ou dans des univers parallèles : tout est permis dans les épisodes de Rick & Morty. <br />
+
+						Chez Cube-Gaming, depuis plusieurs années, des milliers de figurines Funko Pop sont disponibles. Elles sont parfois proposées en exclusivité !</p>
+
+						<button onclick="myFunction()" id="myBtn">Read more</button>
+						<p>Amusez-vous à rechercher vos personnages préférés. Le catalogue complet est incroyable ! <br />
+
+		Vous trouverez facilement votre univers préféré ou votre personnage favori  (Manga, DC Comics, Marvel, séries TV, films, musique...). <br />
+
+		À chaque Pop, son numéro. C'est parti pour posséder la plus belle des collections. <br />
+
+		- Taille : 9 cm <br />
+		- Matière : PVC </p>
+					<br />
+					</div>
+				</div>
+			</section>
+
+			<?php include("../footer_two.php"); ?>
+			
+		</div>
+	</body>
+</html>
